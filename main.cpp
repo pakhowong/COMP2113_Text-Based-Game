2e810cb0649772811e24aa91305b19331139db5d
dlrkgus1012 <62096196+dlrkgus1012@users.noreply.github.com>
2020-05-07 19:00:54 +0900

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,8 +2,28 @@
 
 using namespace std;
 
+//commands 
+void InputHandler::handleInput()
+{
+	if (isPressed(BUTTON_W)) up(); 
+	else if (isPressed(BUTTON_A)) left(); 
+	else if (isPressed(BUTTON_S)) down(); 
+	else if (isPressed(BUTTON_D)) right(); 
+}
+
+class Command 
+{
+public: 
+	virtual ~Command() {}
+	virtual void execute() = 0; 
+}; 
+
+
 int main() {
-	cout << "Hello World!";
-	
-	return 0;
+	//loop for command 
+	while (!done) 
+	{
+		char* command = readCommand(); 
+		handleCommand(command); 
+	}
 }
