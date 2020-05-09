2bde70a81427af7c85353df9b9af5448e4c7fa0d
dlrkgus1012 <62096196+dlrkgus1012@users.noreply.github.com>
2020-05-09 12:39:48 +0900

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,57 +3,79 @@
 using namespace std;
 
 //commands 
-void InputHandler::handleInput()
-{
-	if (isPressed(BUTTON_W)) up()-> execute(); 
-	else if (isPressed(BUTTON_A)) left()-> execute(); 
-	else if (isPressed(BUTTON_S)) down()-> execute(); 
-	else if (isPressed(BUTTON_D)) right()-> execute(); 
-}
-
 class Command 
 {
 public: 
 	virtual ~Command() {}
-	virtual void execute() = 0; 
+	virtual void execute(players) = 0; 
 }; 
 
 class upCommand : public Command
 {
 public: 
-	virtual void execute() { up(); }
+	virtual void execute(players) 
+	{ 
+	players.up(); 
+	}
 };
 
 class leftCommand : public Command
 {
 public: 
-	virtual void execute() { left(); }
+	virtual void execute(players) 
+	{ 
+	players.left(); 
+	}
 };
 
 class downCommand : public Command
 {
 public: 
-	virtual void execute() { down(); }
+	virtual void execute(players) 
+	{
+	players.down(); 
+	}
 };
 
 class rightCommand : public Command
 {
 public: 
-	virtual void execute() { right(); }
+	virtual void execute(players) 
+	{
+	players.right(); 
+	}
 };
 
 class InputHandler
 {
 public: 
 	void handleInput(); 
+
+   //Methods to bind commnands 
 private:
-	Command* buttonX_; 
-	Command* buttonY_; 
+	Command* buttonW_; 
 	Command* buttonA_; 
-	Command* buttonB_; 
+	Command* buttonS_; 
+	Command* buttonD_; 
 };
 
-void 
+Command* InputHandler::handleInput()
+{
+	if (isPressed(BUTTON_W)) return buttonW_();
+	if (isPressed(BUTTON_A)) return buttonA_(); 
+	if (isPressed(BUTTON_S)) return buttonS_(); 
+	if (isPressed(BUTTON_D)) return buttonD_();
+	
+	//nothing pressed, do nothing 
+	return NULL;
+}
+
+//takes command & runs it on the players 
+Command* command = inputHandler.handlerInput(); 
+if (command) 
+{
+	command->execute(players); 
+}
 	
 int main() {
 	//loop for command 
