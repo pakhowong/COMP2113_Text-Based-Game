9883d9bd5167ce06e7f4d8f119d8ab4f16647fd9
dlrkgus1012 <62096196+dlrkgus1012@users.noreply.github.com>
2020-05-09 23:10:45 +0900

    Update stages.cpp

--- stages.cpp
+++ stages.cpp
@@ -65,22 +65,18 @@ void showstage(int height, int width) {
 
 void showbattlestage(int health) {
     refresh();
-    //player
-    cout << "    _O_    " << endl;
-    cout << "  /     \  " << endl;
-    cout << " |==/=\==| " << endl;
-    cout << " |  O O  | " << endl;
-    cout << "  \  V  /  " << endl;
-    cout << "  /`---'\  " << endl;
-    cout << "  O'_:_`O  " << endl;
-    cout << "   -- --   " << endl;
     
-    //monster
-    cout << "   .-.    " << endl;
-    cout << "  (o o)   " << endl;
-    cout << "  | O \   " << endl;
-    cout << "   \   \  " << endl;
-    cout << "    `~~~' " << endl;
+    //player & monster 
+    cout << "    _O_    " << "                 " << endl; 
+    cout << "  /     \  " << "                 " << endl; 
+    cout << " |==/=\==| " << "                 " << "   .-.    " << endl;
+    cout << " |  O O  | " << "                 " << "  (o o)   " << endl;
+    cout << "  \  V  /  " << "                 " << "  | O \   " << endl;
+    cout << "  /`---'\  " << "                 " << "   \   \  " << endl;
+    cout << "  O'_:_`O  " << "                 " << "    `~~~' " << endl;
+    cout << "   -- --   " << "                 " << endl; 
+    
+
     
     cout << "###########" << endl;
     cout << "1: Paper   " << endl;
